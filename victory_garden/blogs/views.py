25ffa209e430fbb1a62d464c3fbaeabf2214f028
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-06 20:48:47 -0500

    Initial setup for blog details page.

--- victory_garden/blogs/views.py
+++ victory_garden/blogs/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
 
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog_details.html'
