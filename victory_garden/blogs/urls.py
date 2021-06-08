f72b65190e4af188aa6109ee4372f9224ff13281
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-07 19:48:32 -0500

    Setup basic header and nav links. Created inital add post page.

--- victory_garden/blogs/urls.py
+++ victory_garden/blogs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, BlogDetailView
+from .views import HomeView, BlogDetailView, AddPostView
 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('', HomeView.as_view(), name='home'),
     path('blog/<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
+    path('add_post/', AddPostView.as_view(), name='add-post'),
 ]
--- victory_garden/blogs/views.py
+++ victory_garden/blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 # def home(request):
 #     return render(request, 'home.html', {})
@@ -12,3 +13,9 @@ class HomeView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = ('title', 'body')
