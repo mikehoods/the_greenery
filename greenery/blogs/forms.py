ab74a9d86571cce1f81ac46ef1560a8edc567ecf
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-10 14:49:35 -0500

    Created Category model

--- greenery/blogs/forms.py
+++ greenery/blogs/forms.py
@@ -1,14 +1,22 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'body')
+        fields = ('title', 'author', 'category', 'body')
 
     widgets = {
-        'title': forms.TextInput(attrs={'class': 'form-input'}),
+        'title': forms.TextInput(attrs={'class': 'form-input', 'placeholder': choices}),
         'author': forms.Select(attrs={'class': 'form-input'}),
+        'category': forms.Select(choices=choice_list, attrs={'class': 'form-input'}),
         'body': forms.Textarea(attrs={'class': 'form-input'}),
         
     }
--- greenery/blogs/models.py
+++ greenery/blogs/models.py
@@ -3,11 +3,24 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "Categories"
+        
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='tests')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
