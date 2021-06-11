11d4383d7b357d29e71e8616a51e349dd9179c09
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-11 01:51:41 -0500

    Initial styling for blog.

--- greenery/blogs/models.py
+++ greenery/blogs/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     date = models.DateField(auto_now_add=True)
-    category = models.CharField(max_length=255, default='tests')
+    category = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
