94a55c8c42a5205595f42541afb016031d7b7e9f
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-15 17:41:12 -0500

    Changed forms.py so author is automatically passed as hidden input.

--- greenery/blogs/forms.py
+++ greenery/blogs/forms.py
@@ -6,9 +6,10 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'author', 'category', 'body')
 
-    # widgets = {
-    #     'title': forms.TextInput(attrs={'class': 'form-input'}),
-    #     'author': forms.Select(attrs={'class': 'form-input'}),
-    #     'category': forms.Select(attrs={'class': 'form-input'}),
-    #     'body': forms.Textarea(attrs={'class': 'form-input'}),
-    # }
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            # 'author': forms.Select(attrs={'class': 'form-input'}),
+            'author': forms.TextInput(attrs={'value': '', 'type': 'hidden'}),
+            'category': forms.Select(attrs={'class': 'form-input'}),
+            'body': forms.Textarea(attrs={'class': 'form-input'}),
+        }
