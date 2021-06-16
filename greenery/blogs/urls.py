01b634e7e969928c15f3c5718e172ed37fb50a89
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-15 20:10:43 -0500

    Setup like button in blog_details.html

--- greenery/blogs/urls.py
+++ greenery/blogs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, BlogDetailView, AddPostView, EditPostView, DeletePostView, AddCategoryView, CategoryView
+from .views import HomeView, BlogDetailView, AddPostView, EditPostView, DeletePostView, AddCategoryView, CategoryView, LikeView
 
 urlpatterns = [
     # path('', views.home, name='home'),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('blog/<int:pk>/delete/', DeletePostView.as_view(), name='delete-post'),
     path('add_category/', AddCategoryView.as_view(), name='add-category'),
     path('category/<str:cat>/', CategoryView, name='category'),
+    path('like/<int:pk>', LikeView, name='like_post'),
 ]
--- greenery/blogs/views.py
+++ greenery/blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Category
 from .forms import PostForm
@@ -6,6 +6,17 @@ from django.urls import reverse_lazy
 
 # def home(request):
 #     return render(request, 'home.html', {})
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+    return redirect('blog-detail', post.pk)
 
 class HomeView(ListView):
     model = Post
