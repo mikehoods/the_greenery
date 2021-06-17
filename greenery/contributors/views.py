8fbaf868d3744d6fadc187c6d1485108c2418192
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-16 21:28:56 -0500

    Extended registration form to include email and first and last name.

--- greenery/contributors/views.py
+++ greenery/contributors/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import SignupForm
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignupForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
