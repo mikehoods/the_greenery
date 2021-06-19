be610a07742f1eaa216dc9f5ceaa66bd77eec8be
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-18 20:37:55 -0500

    Set up edit profile and change password pages.

--- greenery/contributors/urls.py
+++ greenery/contributors/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
-    path('edit_profile/', UserEditView.as_view(), name='edit-profile')
+    path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
+    # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html'), name='change-password')
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html'), name='change-password'),
+    path('password_success', views.password_success, name='password-success'),
 ]
--- greenery/contributors/views.py
+++ greenery/contributors/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignupForm
+from .forms import SignupForm, EditProfileForm
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url= reverse_lazy('password-success')
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
 
 class UserRegisterView(generic.CreateView):
     form_class = SignupForm
@@ -10,7 +18,7 @@ class UserRegisterView(generic.CreateView):
     success_url = reverse_lazy('login')
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 
