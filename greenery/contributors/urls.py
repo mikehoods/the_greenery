f61da1dcb7a7db1a78a82bac0df73a3c6241585e
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-20 20:54:39 -0500

    Created edit_profile_page and revised nav links in base.html

--- greenery/contributors/urls.py
+++ greenery/contributors/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePage
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html'), name='change-password'),
     path('password_success', views.password_success, name='password-success'),
-    path('<int:pk>/profile', ShowProfilePage.as_view(), name='show-profile'),
+    path('<int:pk>/profile', ShowProfilePageView.as_view(), name='show-profile'),
+    path('<int:pk>/edit_profile_page', EditProfilePageView.as_view(), name='edit-profile-page'),
 ]
--- greenery/contributors/views.py
+++ greenery/contributors/views.py
@@ -26,12 +26,19 @@ class UserEditView(UpdateView):
     def get_object(self):
         return self.request.user
 
-class ShowProfilePage(DetailView):
+class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = super(ShowProfilePage, self).get_context_data(*args, **kwargs)
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
         context["page_user"] = page_user
         return context
+
+class EditProfilePageView(UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['user', 'bio', 'profile_pic', 'website_url', 'medium_url']
+    success_url = reverse_lazy('home')
+
