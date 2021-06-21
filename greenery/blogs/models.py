84c26827e9c44f6531e7c775405ee55c91f09673
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-21 14:05:09 -0500

    Created Comment model and setup basic comments in blog_details.html

--- greenery/blogs/models.py
+++ greenery/blogs/models.py
@@ -48,3 +48,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
