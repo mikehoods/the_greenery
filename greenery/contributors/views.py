bb27079c215248fad76d392e6070db1781242c90
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-20 20:20:36 -0500

    Added default user profile pic and contributor profile page.

--- greenery/contributors/views.py
+++ greenery/contributors/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView, UpdateView, DetailView
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignupForm, EditProfileForm
+from blogs.models import Profile
 
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangeForm
@@ -12,15 +13,25 @@ class PasswordsChangeView(PasswordChangeView):
 def password_success(request):
     return render(request, 'registration/password_success.html', {})
 
-class UserRegisterView(generic.CreateView):
+class UserRegisterView(CreateView):
     form_class = SignupForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
-class UserEditView(generic.UpdateView):
+class UserEditView(UpdateView):
     form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 
     def get_object(self):
         return self.request.user
+
+class ShowProfilePage(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ShowProfilePage, self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context["page_user"] = page_user
+        return context
